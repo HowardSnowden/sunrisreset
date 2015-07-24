09cfdff2dfe5cc037da297b9ae16ff86ef76d3b2
HowardSnowden <therealdpyles@gmail.com>
2015-07-24 18:59:36 +0100

    fixed some things

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,4 @@
 class Show < ActiveRecord::Base
- default_scope { where("show_date > ?",  Date.current) }
+ default_scope { where("show_date > ?",  Date.current-1) }
   validates_presence_of  :show_date, :venue
 end
